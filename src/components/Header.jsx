bc20fa43ac188911c8eb91142444ae046809ce4c
odikanwa <odikanwamichael@gmail.com>
2022-09-22 02:32:54 +0100

    completed the dropdown menu and implemented an overlay

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import { BsFillPersonFill } from "react-icons/bs";
 import logo from "../img/logo.png";
+import appleBadge from "../img/download_on_applestore.svg";
+import googleBadge from "../img/download_on_playstore.svg";
 import { NavLink } from "react-router-dom";
 import { MdMoreVert } from "react-icons/md";
 
@@ -63,7 +65,7 @@ const Kebab = styled.div`
   align-self: center;
 `;
 
-const Img = styled.img`
+const LogoIcon = styled.img`
   height: 30px;
   width: 30px;
   margin-left: 10px;
@@ -92,7 +94,7 @@ const Avatar = styled.div`
   display: none;
 `;
 
-export const LinksRibbon = styled.div`
+const LinksRibbon = styled.div`
   height: 9vh;
   width: 100vw;
   margin: 0px;
@@ -143,10 +145,11 @@ const MenuLink = styled(NavLink)`
 `;
 
 const CenterMenu = styled.div`
-  height: 40vh;
+  height: auto;
   width: 70vw;
   margin: 30vh 15vw;
-  display: flex;
+  padding: 30px;
+  display: none;
   align-self: center;
   flex-direction: column;
   justify-content: center;
@@ -155,23 +158,127 @@ const CenterMenu = styled.div`
   color: white;
   position: fixed;
   border-radius: 20px;
-  z-index: 1;
+  z-index: 2;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  -webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
 
-const Text = styled.p``;
+const DropdownLink = styled(MenuLink)`
+  width: 80%;
+  padding: 8px 15px;
+  margin-bottom: 12px;
+  text-align: center;
+  font-weight: bold;
+  background-color: transparent;
+  color: white;
+  border: 1px solid silver;
+`;
+
+const Text = styled.h3`
+  font-weight: bold;
+  margin-top: 20px;
+  margin-bottom: 0px;
+  padding-top: 20px;
+  border-top: 1px solid silver;
+`;
+
+const BadgesDiv = styled.div`
+  height: auto;
+  width: 100%;
+  padding-top: 10px;
+  margin-left: 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Img = styled.img`
+  height: auto;
+  width: 50%;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  background: rgba(0, 0, 0, 0.9); /* Black see-through */
+  height: 100vh;
+  width: 100vw;
+  backdrop-filter: blur(14px);
+  opacity: 1;
+  z-index: 1;
+  display: none;
+`;
+
+// TOD0: Add Google play and applestore icons to 'get the app' and set flex to row.
 
 const Header = () => {
+  const centerMenuRef = useRef();
+  const overlayRef = useRef();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleCenterMenu = () => {
+    setMenuOpen(!menuOpen);
+
+    if (menuOpen === true) {
+      //display center menu and blur page
+      centerMenuRef.current.style.display = "flex";
+      overlayRef.current.style.display = "block";
+    } else {
+      //remove menu and unblur page
+      centerMenuRef.current.style.display = "none";
+      overlayRef.current.style.display = "none";
+    }
+  };
+
+  //Custom hook for closing mobile nav dropdown menu on click ouside the menu
+  const useOnClickOutside = (centerMenuRef, handler) => {
+    useEffect(() => {
+      const listener = (event) => {
+        //Do nothing if clicking ref's element or descendant elements
+        if (
+          !centerMenuRef.current ||
+          centerMenuRef.current.contains(event.target)
+        ) {
+          return;
+        } else {
+          //exit menu
+          centerMenuRef.current.style.display = "none";
+          overlayRef.current.style.display = "none";
+        }
+        //Keep menu fixed for tablet devices
+        // if (window.screen.width > 767) {
+        //   buttonRef.current.style.display = "none";
+        //   menuRef.current.style.display = "inline-flex";
+        // }
+        // handler(event);
+      };
+      //document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
+      return () => {
+        //document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+      };
+    }, [centerMenuRef, handler]);
+  };
+  //call onclickOutside hook
+  useOnClickOutside(centerMenuRef, () => setMenuOpen(true));
+
   return (
     <>
       <GlobalStyle />
       <Container>
+        <Overlay ref={overlayRef} />
         <Wrapper>
           <Menu>
-            <Kebab>
+            <Kebab onClick={toggleCenterMenu}>
               <MdMoreVert style={{ height: "30px", width: "30px" }} />
             </Kebab>
             <Logo>
-              <Img src={logo} />
+              <LogoIcon src={logo} />
               <LogoText>Fyyur</LogoText>
             </Logo>
           </Menu>
@@ -187,14 +294,23 @@ const Header = () => {
             <MenuLink to="../artists/">Artists</MenuLink>
           </Button>
           <Button>
-            <MenuLink to="../venues">Venues</MenuLink>
+            <MenuLink to="../venues/">Venues</MenuLink>
           </Button>
         </LinksRibbon>
-        <CenterMenu>
-          <Text>Login</Text>
-          <Text>Post Show</Text>
-          <Text>Post Venue</Text>
-          <Text>Get the App</Text>
+        <CenterMenu ref={centerMenuRef}>
+          <DropdownLink
+            to="../forms/loginform/"
+            style={{ backgroundColor: "#ff0800", border: "none" }}
+          >
+            Login
+          </DropdownLink>
+          <DropdownLink to="../forms/loginform/">Post Show</DropdownLink>
+          <DropdownLink to="../forms/loginform/">Post Venue</DropdownLink>
+          <Text>Get the best experience on the app.</Text>
+          <BadgesDiv>
+            <Img src={googleBadge} />
+            <Img src={appleBadge} />
+          </BadgesDiv>
         </CenterMenu>
       </Container>
     </>
